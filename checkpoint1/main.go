c260e3471d983c92f5990ec20d12bd45f7295cab
SUIYUELIANYI <1911401642@qq.com>
2021-11-25 22:15:13 +0800

    test

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -15,11 +15,6 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(req)
-	//resp, err := req.SendRequest()
-	//resp.ShowHeader()
-	//value, err := resp.GetHeader("Passport")
-	//req.AddHeader("Passport",value[0])
     resp1, err := req.SendRequest()
 	resp1.ShowBody()
 }
