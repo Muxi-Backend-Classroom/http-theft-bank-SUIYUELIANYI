c874befde270f1853fe32e836b28029b5c8b694c
SUIYUELIANYI <92309370+SUIYUELIANYI@users.noreply.github.com>
2021-11-28 08:53:01 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -2,25 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/Grand-Theft-Auto-In-CCNU-MUXI/hacker-support/httptool"
+	"io/ioutil"
+	"net/http"
 )
 
 func main() {
-	req, err := httptool.NewRequest(
-		httptool.GETMETHOD,
-		"http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code",
+	/*req, err := httptool.NewRequest(
+		"",
+		"",
 		"",
 		httptool.DEFAULT, // 这里可能不是 DEFAULT，自己去翻阅文档
 	)
 	if err != nil {
 		fmt.Println(err)
 	}
-	//fmt.Println(req)
-	resp, err := req.SendRequest()
-	//resp.ShowHeader()
-	value, err := resp.GetHeader("Passport")
-	req.AddHeader("Passport",value[0])
-	req.AddHeader("Code","200")
-    resp1, err := req.SendRequest()
-	resp1.ShowBody()
+	fmt.Println(req)*/
+
+	// write your code below
+	// ...
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code", nil)
+	req.Header.Add("Code", "120")
+	res, _ := client.Do(req)
+	by, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(by))
+	/*passport := res.Header["Passport"][0]
+	req, _ = http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/secret_key", nil)
+	req.Header.Add("Code", "120")
+	req.Header.Add("Passport", passport)
+	res, _ = client.Do(req)
+	fmt.Println(res.Header)
+	content, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(content))*/
 }
