0259f64beacbeb4322b663b23f5d2e9e6e36e63f
SUIYUELIANYI <92309370+SUIYUELIANYI@users.noreply.github.com>
2021-11-28 08:45:34 +0800

    Update main.go

--- checkpoint1/main.go
+++ checkpoint1/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"github.com/Grand-Theft-Auto-In-CCNU-MUXI/hacker-support/httptool"
 )
 
 func main() {
-	client := &http.Client{}
-	req, _ := http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code", nil)
-	req.Header.Add("Code", "120")
-	res, _ := client.Do(req)
-	by, _ := ioutil.ReadAll(res.Body)
-	fmt.Println(string(by))
+	req, err := httptool.NewRequest(
+		httptool.GETMETHOD,
+		"http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/code",
+		"",
+		httptool.DEFAULT, // 这里可能不是 DEFAULT，自己去翻阅文档
+	)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(req)
+	resp, err := req.SendRequest()
+	//resp.ShowHeader()
+	value, err := resp.GetHeader("Passport")
+	req.AddHeader("Passport",value[0])
+	req.AddHeader("Code","200")
+    resp1, err := req.SendRequest()
+	resp1.ShowBody()
 }
