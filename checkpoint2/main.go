aa82af7cb0530763f2e7b422500cc6a280a6ae7b
SUIYUELIANYI <92309370+SUIYUELIANYI@users.noreply.github.com>
2021-11-30 21:57:13 +0800

    Update main.go

--- checkpoint2/main.go
+++ checkpoint2/main.go
@@ -7,18 +7,6 @@ import (
 )
 
 func main() {
-	/*req, err := httptool.NewRequest(
-		"",
-		"",
-		"",
-		httptool.DEFAULT, // 这里可能不是 DEFAULT，自己去翻阅文档
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(req)
-	// write your code below
-	// ...*/
 	client := &http.Client{}
 	passport := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoicWlhbnJlbiIsImlhdCI6MTYzODI4MDQ4NSwibmJmIjoxNjM4MjgwNDg1fQ.Q4kTf6FrWVNjMrDe2eV_JdC5siD7KJtMQgzb4u4jM9o"
 	req, _ := http.NewRequest("GET", "http://http-theft-bank.gtainccnu.muxixyz.com/api/v1/organization/secret_key", nil)
